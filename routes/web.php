b296019a3e216a1499517882fd95afdfe4329f3b
Dario <dario.stach@gmail.com>
2020-10-07 09:28:15 -0300

    Hero new models and seeders

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ Route::get('/home/{name}', function ($name) {
     return view('home', ['name' => $name]);
     //return 'Hola '. $name .' como va?';
 })->where('name', '[A-Za-z]+');
+
+//Route::get('/admin', 'AdminController@index');
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('/', 'App\Http\Controllers\AdminController@index')->name('admin'); // works fine
+    Route::get('heroes', 'App\Http\Controllers\HeroController@index')->name('admin.heroes');
+    Route::get('items', 'App\Http\Controllers\ItemController@index')->name('admin.items');
+    Route::get('enemies', 'App\Http\Controllers\EnemyController@index')->name('admin.enemies');
+});
